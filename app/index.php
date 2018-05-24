33ea572b494c55cd89ba5b9013ee40ceea71abba
francerz <francerz@users.noreply.github.com>
2018-05-23 23:33:18 -0500

    Index con content view.

--- app/index.php
+++ app/index.php
@@ -2,7 +2,9 @@
 <?php include ROOT_DIR.'/struct/top.php'; ?>
 <?php include ROOT_DIR.'/struct/header.php'; ?>
 <main>
-    <h1>Título principal</h1>
-    Aquí va el contenido.
+    <div class='content-view'>
+        <h1>Título principal</h1>
+        Aquí va el contenido.
+    </div>
 </main>
 <?php include ROOT_DIR.'/struct/bottom.php'; ?>
